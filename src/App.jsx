e9342efbe68082aebeaf4918930c0360c7221358
TanayPatil1503 <tanaypatil1503@gmail.com>
2026-02-17 08:23:48 +0530

    Initial commit - React Vite project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline p-0 m-0">nighthowl</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
